da97757257b881e3c468546bbbc75477b5febba4
sleivav <sergio.leiva@ing.uchile.cl>
2019-08-29 16:51:37 -0400

    clase miércoles 28

--- demo_aux/admin.py
+++ demo_aux/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from demo_aux.models import FarmAnimal
+from demo_aux.models import FarmAnimal, Owner, Farm
 
 admin.site.register(FarmAnimal)
+admin.site.register(Owner)
+admin.site.register(Farm)
--- demo_aux/models.py
+++ demo_aux/models.py
@@ -1,9 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Owner(models.Model):
+    user = models.OneToOneField(User, models.CASCADE)
+    name = models.CharField(max_length=40)
+
+
+class Farm(models.Model):
+    name = models.CharField(max_length=30)
+    owner = models.ForeignKey(Owner, models.SET_NULL, null=True, blank=True)
+
 
 # Create your models here.
 class FarmAnimal(models.Model):
     animal_name = models.CharField(max_length=15)
     number_legs = models.IntegerField(default=4)
+    farm = models.ForeignKey(Farm, related_name='animal',
+                             on_delete=models.SET_NULL,
+                             blank=True,
+                             null=True)
+    image = models.ImageField(upload_to='fotos', default='fotos/404.jpg')
 
     def __str__(self):
         return self.animal_name
